1a0ebf3696c88ee34af7d2e89d2c145c8860229b
Elías Hauksson <hello@eliashauksson.com>
2024-11-22 10:23:42 +0100

    update emacs config

--- config/emacs/early-init.el
+++ config/emacs/early-init.el
@@ -1,8 +1,34 @@
-(setq site-run-file nil
-      inhibit-default-init t
-      gc-cons-threshold most-positive-fixnum
-      package-enable-at-startup nil
-      frame-resize-pixelwise t)
+;;; early-init.el --- Early initialization settings for Emacs
 
+;;; Commentary:
+;; This file contains early initialization settings for Emacs.
+;; It is loaded before the package system and GUI is initialized.
+
+;; features:
+;; - disable gui elements like menu bar, scroll bar, tool bar, and tooltips.
+;; - sets the default font to "Iosevka Term Nerd Font".
+;; - adjusts garbage collection threshold for faster startup.
+;; - ensures pixel-perfect window resizing.
+
+;;; Code:
+
+;; turn off unwanted gui elements
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+
+;; set default font
+(add-to-list 'default-frame-alist
+	     '(font . "Iosevka Term Nerd Font 13"))
+
+;; increase garbage collection threshold for faster startup
+(setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'after-init-hook
 	  #'(lambda () (setq gc-cons-threshold (* 8 1024 1024))))
+
+;; make windows resize pixel perfect and not after line height
+(setq frame-resize-pixelwise t)
+
+(provide 'early-init)
+;;; early-init ends here
--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,149 +1,51 @@
-(defvar my/init-start-time (current-time) "Time when init.el was started")
-(defvar my/section-start-time (current-time) "Time when section was started")
-(defun my/report-time (section)
-  (message "%-36s %.2fs"
-	   (concat section " " "section time: ")
-	   (float-time (time-subtract (current-time) my/section-start-time))))
+;;; init.el --- Main initialization file for Emacs
 
-(server-start)
+;;; Commentary:
+;; This is the main configuration file for Emacs.  It sets up core functionality
+;; and loads additional configurations.
 
-;; Package management
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; features:
+;; - starts the Emacs server.
+;; - load configuration files for settings and package management.
+;; - toggles a default theme using function my/toggle-theme (if defined).
 
-(setq package-list
-      '(vc-backup
-        rainbow-delimiters
-        elpy
-        company))
+;; additional configuration files
+;; - settings.el contains general Emacs settings.
+;; - packages.el handles package installation and configuration.
 
-(dolist (package package-list)
-  (straight-use-package package))
+;;; Code:
 
-(straight-use-package
- '(nano-theme :type git :host github :repo "rougier/nano-theme"))
+;; start server
+(require 'server)
+(unless (server-running-p)
+    (server-start))
 
-;; disable some default startup things
-(setq-default
- inhibit-startup-screen t
- inhibit-startup-message t
- inhibit-startup-echo-area-message t
- initial-scratch-message ""
- initial-buffer-choice t)
+(defun my/load-config-file (file)
+  "Load a configuration FILE from `~/.config/emacs/`."
+  (load (expand-file-name file "~/.config/emacs/")))
 
-;; utf-8 encoding
-(set-default-coding-systems 'utf-8)
-(prefer-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
+(my/load-config-file "settings")
+(my/load-config-file "packages")
 
-;; recovery
-(setq auto-save-list-file-prefix
-      (expand-file-name ".auto-save-list/.saves-" user-emacs-directory)
-      auto-save-default t
-      auto-save-timout 20
-      auto-save-interval 200)
+;; toggle theme
+(when (fboundp 'my/toggle-theme)
+  (my/toggle-theme))
 
-;; backups
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name "backups" user-emacs-directory)))
-      make-backup-files t
-      vc-make-backup-files t
-      backup-by-copying t
-      version-control t
-      delete-old-versions t
-      kept-old-versions 6
-      kept-new-versions 9
-      delete-by-moving-to-trash t)
+;; user defined init.el ends here
 
-(setq initial-major-mode 'text-mode
-      default-major-mode 'text-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(flycheck copilot company company-mode doom-modeline counsel rainbow-delimiters doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-(setq-default indent-tabs-mode nil
-	      tab-width 4)
-
-(setq my-line-numbers-exceptions '(eshell-mode
-				   term-mode))
-(defun my-conditional-line-numbers ()
-  (unless (member major-mode my-line-numbers-exceptions)
-    (display-line-numbers-mode 1)))
-(add-hook 'after-change-major-mode-hook 'my-conditional-line-numbers)
-
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-(my/report-time "Core")
-(setq my/section-start-time (current-time))
-
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-
-(setq-default window-divider-default-right-width 24
-	      window-divider-default-places 'right-only
-	      left-margin-width 0
-	      right-margin-width 0
-	      window-combination-resize nil)
-(window-divider-mode 1)
-
-(blink-cursor-mode 0)
-
-(require 'nano-theme)
-
-(defun is-dark-theme? ()
-  (let ((bashrc-content (shell-command-to-string "cat ~/.bashrc"))
-        (theme-regex "^export THEME=\\(.*\\)$"))
-    (if (string-match theme-regex bashrc-content)
-        (string= "DARK" (match-string 1 bashrc-content))
-      nil)))
-
-(defun my-toggle-theme-function ()
-  (interactive)
-  (if (is-dark-theme?) (nano-dark) (nano-light)))
-
-(my-toggle-theme-function)
-
-(setq default-frame-alist
-      (append (list
-	       '(font . "Iosevka Term Nerd Font 13")
-	       '(vertical-scroll-bars . nil)
-	       '(internal-border-width . 12)
-	       '(left-fringe . 1)
-	       '(tool-bar-lines . 0)
-	       '(menu-bar-lines . 0))))
-
-(setq my-rainbow-delimiters-modes '(clojure-mode
-                                    emacs-lisp-mode))
-(dolist (mode my-rainbow-delimiters-modes)
-  (add-hook (intern (concat (symbol-name mode) "-hook"))
-            'rainbow-delimiters-mode))
-
-(my/report-time "UI")
-(setq my/section-start-time (current-time))
-
-(global-company-mode)
-
-(elpy-enable)
-
-(my/report-time "Programming")
-(setq my/section-start-time (current-time))
-
-(let ((init-time (float-time (time-subtract (current-time) my/init-start-time)))
-      (total-time (string-to-number (emacs-init-time))))
-
-  (message "---------------------------------------------------------------")
-  (message "Initialization time:                 %.2fs (+ %.2f system time)"
-           init-time (- total-time init-time))
-  (message "---------------------------------------------------------------"))
+(provide 'init)
+;;; init.el ends here
