365de15e942fc0093b33bbd7058621328e7e6835
Elías Hauksson <eliashauksson@protonmail.com>
2025-12-14 17:48:45 +0100

    updated emacs config (focus for c++ development and org-mode writing)

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,51 +1,26 @@
-;;; init.el --- Main initialization file for Emacs
+;;; init.el -*- lexical-binding: t -*-
 
-;;; Commentary:
-;; This is the main configuration file for Emacs.  It sets up core functionality
-;; and loads additional configurations.
+;; Disable default package management
+(setq package-enable-at-startup nil)
 
-;; features:
-;; - starts the Emacs server.
-;; - load configuration files for settings and package management.
-;; - toggles a default theme using function my/toggle-theme (if defined).
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el"
+			 user-emacs-directory))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; additional configuration files
-;; - settings.el contains general Emacs settings.
-;; - packages.el handles package installation and configuration.
+;; Install and load Org
+(straight-use-package 'org)
 
-;;; Code:
-
-;; start server
-(require 'server)
-(unless (server-running-p)
-    (server-start))
-
-(defun my/load-config-file (file)
-  "Load a configuration FILE from `~/.config/emacs/`."
-  (load (expand-file-name file "~/.config/emacs/")))
-
-(my/load-config-file "settings")
-(my/load-config-file "packages")
-
-;; toggle theme
-(when (fboundp 'my/toggle-theme)
-  (my/toggle-theme))
-
-;; user defined init.el ends here
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(flycheck copilot company company-mode doom-modeline counsel rainbow-delimiters doom-themes use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(provide 'init)
-;;; init.el ends here
+;; Now load the literate config
+(org-babel-load-file
+ (expand-file-name "config.org" user-emacs-directory))
